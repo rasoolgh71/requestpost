9e19626615b614826e4be1614bcaccbefc141f58
rasoolghanna <rasoolghanna@gmail.com>
2017-12-12 12:43:52 +0330

    Feature:#5 completet subprocess

--- task/views.py
+++ task/views.py
@@ -35,9 +35,30 @@ class GenerateRandomMessageView(FormView):   # craete work celry and random mess
         return redirect('show')
 
 def is_disk(request):    # show volumn-name- modify date directory
+    list=[]
+    dict={}
+    data={}
     #disk = subprocess.Popen(["ls", "-l"],stdout=subprocess.PIPE, shell=True)
    # disk = disk.communicate()
-    process = subprocess.Popen(["ls -l  |  awk  '{ print $5,$6,$7,$8, $9 }'"], cwd='/home/parspooyesh-kashan/test-1',
+    process = subprocess.Popen(["ls -l  |  awk  '{ print $5,$6 $8 $7 , $9 }'"], cwd='/home/parspooyesh-kashan/test-1',
                               stdin=subprocess.PIPE, stdout=subprocess.PIPE,shell=True)
-    disk = process.communicate()
-    return render(request,'task/disk.html',context={'disk':disk})
+    disk,err = process.communicate()
+    disk=disk.decode('utf-8').split('\n')
+    #disk = disk.decode('utf-8')
+    #disk1=[disk.strip() for n in disk]
+    #disk1=[disk.split() for n in disk]
+
+    #list=list.append(disk)
+    for i in disk:
+        data[i]=i.split(' ')
+        print(data[i])
+        #data.pop()
+
+        #print(type(dict1))
+
+       # print(data)
+        print("data:",data)
+        #pass
+
+    #lits1=list.split('\n')
+    return render(request,'task/disk.html',context={'disk':disk,'data':data})
